d2e3a01653cf9a9968876c2daee0e9f6109fbc02
Joaco Alvarez <joaquinaalvarez91@gmail.com>
2023-10-03 14:48:21 -0300

    Entrega ADONET Acceso a Datos

---
+++ CapaNegocio/CN-Usuario.cs
@@ -0,0 +1,20 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class CN_Usuario
+    {
+        
+
+        public List<Usuario> ListarUsuarios()
+        {
+            return UsuarioData.ListarUsuarios();
+        }
+    }
+}
